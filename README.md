e380ee6a3a66aa8eb2830280295b887986aec3fc
Stefan Stammberger <some.fusion@gmail.com>
2017-06-25 18:17:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Neighborhood Map - A Udacity Nanodegree project
+Author: Stefan Stammberger
+
+### How to run
+Install nodejs and npm
+
+Run commands:
+* npm install
+* npm run dev-server
+* Open http://localhost:9000/ in a browser
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require("path");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2015"]
+        }
+      }
+    ]
+  },
+
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".js"]
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
+  }
+};
