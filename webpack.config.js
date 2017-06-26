645f7666aa1347d2c6414b9da17ef7b62a866c93
Stefan Stammberger <some.fusion@gmail.com>
2017-06-26 20:15:59 +0200

    feat: load bootstrap via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/js/index.js",
+  entry: ["bootstrap-loader", "./src/js/index.js"],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
@@ -34,5 +35,26 @@ module.exports = {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9000
-  }
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Tether: "tether",
+      "window.Tether": "tether",
+      Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+      Button: "exports-loader?Button!bootstrap/js/dist/button",
+      Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+      Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+      Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+      Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+      Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+      Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+      Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+      Util: "exports-loader?Util!bootstrap/js/dist/util"
+    })
+  ]
 };
