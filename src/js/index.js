7a9405700fbd897aa106331e31b9e1c20b811109
Stefan Stammberger <some.fusion@gmail.com>
2017-06-27 19:24:15 +0200

    feat: add Google Maps API

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,27 @@
-console.log("hello index.js");
+const keys = require("./keys.js");
+
+var loadGoogleMapsApi = require("load-google-maps-api-2");
+loadGoogleMapsApi.key = keys.mapsAPIKey;
+
+// loads asynchronously
+loadGoogleMapsApi()
+  .then(function(googleMaps) {
+    var myLatlng = { lat: -25.363, lng: 131.044 };
+
+    var map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: myLatlng
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: "Click to zoom"
+    });
+
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 
 import "../css/main.css";
