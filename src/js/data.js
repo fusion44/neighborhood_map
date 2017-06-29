51f82677d0b6f406164a27a15892f5a723719f46
Stefan Stammberger <some.fusion@gmail.com>
2017-06-29 07:20:00 +0200

    feat: add a simple UI

---
+++ src/js/data.js
@@ -0,0 +1,65 @@
+exports.locations = [
+  {
+    latlong: {
+      lat: 47.557604,
+      lng: 10.749799
+    },
+    title: "Schloss Neuschwanstein"
+  },
+  {
+    latlong: {
+      lat: 50.941265,
+      lng: 6.958271
+    },
+    title: "Kölner Dom"
+  },
+  {
+    latlong: {
+      lat: 52.516275,
+      lng: 13.377699
+    },
+    title: "Brandenburger Tor"
+  },
+  {
+    latlong: {
+      lat: 50.961912,
+      lng: 14.073188
+    },
+    title: "Basteigebiet"
+  },
+  {
+    latlong: {
+      lat: 47.870997,
+      lng: 12.453865
+    },
+    title: "Chiemsee"
+  },
+  {
+    latlong: {
+      lat: 53.57803,
+      lng: 6.949848
+    },
+    title: "Wattenmeer"
+  },
+  {
+    latlong: {
+      lat: 51.749997,
+      lng: 10.633354
+    },
+    title: "Harz"
+  },
+  {
+    latlong: {
+      lat: 48.138624,
+      lng: 11.573628
+    },
+    title: "Frauenkirche"
+  },
+  {
+    latlong: {
+      lat: 47.421048,
+      lng: 10.985397
+    },
+    title: "Zugspitze"
+  }
+];
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 const keys = require("./keys.js");
+const data = require("./data.js");
 
 var loadGoogleMapsApi = require("load-google-maps-api-2");
 loadGoogleMapsApi.key = keys.mapsAPIKey;
@@ -6,19 +7,19 @@ loadGoogleMapsApi.key = keys.mapsAPIKey;
 // loads asynchronously
 loadGoogleMapsApi()
   .then(function(googleMaps) {
-    var myLatlng = { lat: -25.363, lng: 131.044 };
-
     var map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: myLatlng
+      zoom: 6,
+      center: { lat: 51, lng: 9 },
+      disableDefaultUI: true
     });
 
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      title: "Click to zoom"
-    });
-
+    data.locations.forEach(function(poi) {
+      var marker = new google.maps.Marker({
+        position: poi.latlong,
+        map: map,
+        title: poi.title
+      });
+    }, this);
   })
   .catch(function(err) {
     console.log(err);
